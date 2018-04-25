e8609f5df6c3a2b4e66f717ba19778778544e5ee
Paul Cromey <paul.cromey@bbc.co.uk>
2018-04-25 09:17:17 +0100

    Added Spring Security

---
+++ src/main/java/com/cromey/identity/webflux/oauth/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.cromey.identity.webflux.oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .pathMatchers("/docs").permitAll()
+                .pathMatchers("/profiles").hasAuthority("ROLE_USER")
+                .anyExchange().authenticated()
+                .and().formLogin()
+                .and().build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new MapReactiveUserDetailsService(user);
+    }
+
+}
